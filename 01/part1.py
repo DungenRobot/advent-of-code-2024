48f1e224f8470291ee34026ded36943116c92547
Dungen <104450078+DungenRobot@users.noreply.github.com>
2024-12-01 00:29:16 -0500

    day1

---
+++ 01/part1.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    list1 = []
+    list2 = []
+
+    with open("input") as f:
+        for line in f:
+            num1, num2 = line.split("   ")
+            list1.append(int(num1))
+            list2.append(int(num2))
+
+    list1.sort()
+    list2.sort()
+
+    sum = 0
+
+    for i in range(len(list1)):
+        sum += abs(list1[i] - list2[i])
+
+    print(sum)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ 01/part2.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    list1 = []
+    list2 = {}
+
+    with open("input") as f:
+        for line in f:
+            num1, num2 = line.split("   ")
+            list1.append(int(num1))
+
+            list2[int(num2)] = list2.get(int(num2), 0) + 1
+
+    sum = 0
+
+    for num in list1:
+        sum += num * list2.get(num, 0)
+
+    print(sum)
+
+
+
+
+if __name__ == "__main__":
+    main()
